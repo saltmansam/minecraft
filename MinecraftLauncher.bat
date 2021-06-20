edddb9d9b7c4871581d28012430f9b34bb37c45c
saltmansam <saltysamsoup@gmail.com>
2021-06-20 13:58:23 -0400

    Sun 06/20/2021

---
+++ MinecraftLauncher.bat
@@ -0,0 +1,11 @@
+git pull origin master
+
+
+
+cd C:\Program Files (x86)\Minecraft Launcher
+MinecraftLauncher.exe
+
+cd c:\Users\salty\AppData\Roaming\.minecraft\saves
+git add .
+git commit -m "%date%"
+git push origin master
